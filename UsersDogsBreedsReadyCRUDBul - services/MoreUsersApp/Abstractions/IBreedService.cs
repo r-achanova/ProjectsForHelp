6831231e740c5d999b9932c0e91764e65462f690
r-achanova <r.achanova@pmg-pernik.com>
2022-05-06 06:38:38 +0300

    Add practical task with services

---
+++ UsersDogsBreedsReadyCRUDBul - services/MoreUsersApp/Abstractions/IBreedService.cs
@@ -0,0 +1,17 @@
+﻿using MoreUsersApp.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MoreUsersApp.Abstractions
+{
+   public interface IBreedService
+    {
+        
+        List<Breed> GetBreeds();
+        Breed GetBreedById(int breedId);
+        List<Dog> GetDogsByBreed(int breedId);
+        
+    }
+}
---
+++ UsersDogsBreedsReadyCRUDBul - services/MoreUsersApp/Services/BreedService.cs
@@ -0,0 +1,41 @@
+﻿using MoreUsersApp.Abstractions;
+using MoreUsersApp.Data;
+using MoreUsersApp.Entities;
+using MoreUsersApp.Models.Breed;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MoreUsersApp.Services
+{
+    public class BreedService : IBreedService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public BreedService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public Breed GetBreedById(int breedId)
+        {
+            return _context.Breeds.Find(breedId);
+        }
+
+        public List<Breed> GetBreeds()
+        {
+            List<Breed> breeds = _context.Breeds.ToList();
+            return breeds;
+        }
+
+       
+        public List<Dog> GetDogsByBreed(int breedId)
+        {
+            return _context.Dogs
+                .Where(x => x.BreedId ==
+                breedId)
+                .ToList();
+        }
+    }
+}
